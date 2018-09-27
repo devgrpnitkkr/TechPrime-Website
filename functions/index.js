ac543ac82653fd86b134113be1e4e86d2eff98dd
Sushant <write2su756agg@gmail.com>
2018-09-28 01:47:27 +0530

    cleaned the code a bit more

--- functions/index.js
+++ functions/index.js
@@ -3,36 +3,46 @@ const admin = require('firebase-admin');
 const jwt = require('jsonwebtoken');
 
 admin.initializeApp();
-var database=admin.database();
+let database = admin.database();
+
 
 exports.googleLogin = functions.https.onRequest(function(req,response){
+    let accToken = req.query.accessToken;
     const request = require('request');
-    request('https://www.googleapis.com/plus/v1/people/me?access_token='+req.query.accessToken, { json: true }, (err, res, body) => {
-        if (err) { return console.log(err);
+    request('https://www.googleapis.com/plus/v1/people/me?access_token='+accToken, { json: true }, (err, res, body) => {
+        let data;
+        if(err)
+        {
+          return console.log(err);
         }
         console.log(body);
-        if(body.error!=null){
+        if(body.error != null)
+        {
             console.log('error in accessToken');
             data={
                 authenticatedRequest:false,
             };
             return response.json(data);
         }
-        var email1 =body.emails[0].value;
-        var email=email1.replace(/\./g,',');
+
+        let email1 = body.emails[0].value;
+        let email = email1.replace(/\./g,',');
         console.log(email);
-        var ref =database.ref();
+        let email_child = "users/"+email;
+        let ref = database.ref();
+
         ref.once('value',function(snapshot){
-            if(snapshot.hasChild('users/'+email))
+            if(snapshot.hasChild(email_child))
             {
                 console.log('present');
-                var reff=database.ref('users/'+email);
-                var onB;
-                reff.once('value',function(snap){
+                let reff = database.ref(email_child);
+                let onB;
+                reff.once('value',function(snap)
+                {
                     console.log(snap.val().onBoard);
                     onB=snap.val().onBoard;
                     console.log(onB);
-                    var data={
+                    data={
                         onBoard:onB,
                         authenticatedRequest:true,
                         isRegistered:true,
@@ -41,11 +51,11 @@ exports.googleLogin = functions.https.onRequest(function(req,response){
                     const token = jwt.sign( data ,"abab", { expiresIn: "12h"});
                     response.json(token);
                 });
-                console.log('onboard is'+onB);
-
+                 console.log('onboard is'+onB);
             }
-            else {
-                database.ref('users/'+email).set({
+            else
+            {
+                database.ref(email_child).set({
                     onBoard:false,
                     email: body.emails[0].value,
                     name: body.name.givenName+" "+body.name.familyName,
@@ -63,36 +73,46 @@ exports.googleLogin = functions.https.onRequest(function(req,response){
     });
 });
 
-var express=require('express');
-var bodyParser= require('body-parser');
-var app=express();
+
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
 app.use(bodyParser.urlencoded({extended:false}));
-app.post('/',function(req,response){
+
+
+app.post('/',function(req,response)
+{
     if(req.body.accessToken === undefined || req.body.accessToken === '') response.json({error: true});
-    else {
+    else
+    {
         jwt.verify(req.body.accessToken, "abab", (err, data) => {
             if (err) {
                 response.json({error: true});
             }
-            else {
-                if (req.body.phone === undefined || req.body.college === undefined || req.body.year === undefined) {
-                    return response.send('please pass valid/complete url parameters');
-                }
+            else
+            {
                 if (data.error != null) {
                     return response.json({
                         authenticatedRequest: false,
                     });
                 }
-                else {
-                    var email1 = data.body.emails[0].value;
+                if (req.body.phone === undefined || req.body.college === undefined || req.body.year === undefined) {
+                    return response.send('please pass valid/complete url parameters');
+                }
+                else
+                {
+                    let email1 = data.body.emails[0].value;
                     console.log(email1);
-                    var email = email1.replace(/\./g, ',');
-                    var ref = database.ref('users/');
+                    let email = email1.replace(/\./g, ',');
+                    let ref = database.ref('users/');
+                    let email_child = "users/"+email;
                     ref.once('value', function (snapshot) {
                         console.log(snapshot.val());
-                        if (snapshot.hasChild(email)) {
+                        if (snapshot.hasChild(email))
+                        {
                             console.log('present');
-                            database.ref('users/' + email).update({
+                            database.ref(email_child).update({
                                 onBoard: true,
                                 phone: req.body.phone,
                                 college: req.body.college,
@@ -106,4 +126,5 @@ app.post('/',function(req,response){
         });
     }
 });
+
 exports.signUp = functions.https.onRequest(app);
